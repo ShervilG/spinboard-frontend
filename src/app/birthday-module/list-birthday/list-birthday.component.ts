3990b4ac26a1342644798d433d6738d3a63242e3
Shervil Gupta <35729671+ShervilG@users.noreply.github.com>
2023-04-07 20:40:30 +0530

    Added Birthday module and a sample component (#8)
    * Added Birthday module and a sample component
    
    * Changed CI/CD to act on push

---
+++ src/app/birthday-module/list-birthday/list-birthday.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-birthday',
+  templateUrl: './list-birthday.component.html',
+  styleUrls: ['./list-birthday.component.scss']
+})
+export class ListBirthdayComponent {
+
+}
